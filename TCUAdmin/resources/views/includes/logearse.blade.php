88bb031eda82de8ac40b310e506508ccd0a36aa6
David Bermudez <davidpiber@hotmail.com>
2017-10-01 21:09:26 -0600

    formualrios de registro y login

---
+++ TCUAdmin/resources/views/includes/logearse.blade.php
@@ -0,0 +1,14 @@
+
+<div class="col-md-6">
+    <h3>Log In</h3>
+    <form action="#" method="post">
+        <div class="form-group">
+            <label for="email">Correo Personal:</label>
+            <input class="form-control" name="email" type="email">
+        </div>
+        <div class="form-group">
+            <label for="nombre">Password:</label>
+            <input class="form-control" type="password" name="password">
+        </div>
+    </form>
+</div>
---
+++ TCUAdmin/resources/views/includes/registro.blade.php
@@ -0,0 +1,52 @@
+<div class="col-md-6">
+    <h3>Registrarse</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="nombre">Nombre:</label>
+                    <input class="form-control" type="text" name="nombre">
+                </div>
+                <div class="form-group">
+                    <label for="nombre">Primer Apellido:</label>
+                    <input class="form-control" type="text" name="primer_apellido">
+                </div>
+                <div class="form-group">
+                    <label for="nombre">Segundo Apellido:</label>
+                    <input class="form-control" type="text" name="segundo_apellido">
+                </div>
+                <div class="form-group">
+                    <label for="nombre">Cedula:</label>
+                    <input class="form-control" type="text" name="cedula">
+                </div>
+                <div class="form-group">
+                    <label for="nombre">Carnet Universidad:</label>
+                    <input class="form-control" type="text" name="carnet_universidad">
+                </div>
+                <div class="form-group">
+                    <label for="nombre">Correo Universidad:</label>
+                    <input class="form-control" type="email" name="correo_universidad">
+                </div>
+                <div class="form-group">
+                    <label for="email">Correo Personal:</label>
+                    <input class="form-control" type="email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="nombre">Password:</label>
+                    <input class="form-control" type="password" name="password">
+                </div>
+                <div class="form-group">
+                    <label for="genero">Genero:</label>
+                    <select name="genero" class="form-control">
+                        <option value="masculino">Masculino</option>
+                        <option value="femenino">Femenino</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="sede">Sede:</label>
+                    <select name="sede" class="form-control">
+                        <option value="heredia">Heredia</option>
+                        <option value="san_pedro">San Pedro</option>
+                    </select>
+            </div>
+            </form>
+        </div>
+    </div>
