bf223d8a09a761205a3547cd742b9bf5457e4eca
David Bermudez <davidpiber@hotmail.com>
2017-10-15 16:30:55 -0600

    changing emal to corre_personal

--- TCUAdmin/resources/views/includes/registro.blade.php
+++ TCUAdmin/resources/views/includes/registro.blade.php
@@ -27,7 +27,7 @@
                 </div>
                 <div class="form-group">
                     <label for="email">Correo Personal:</label>
-                    <input class="form-control" type="email" name="email">
+                    <input class="form-control" type="email" name="correo_personal">
                 </div>
                 <div class="form-group">
                     <label for="nombre">Password:</label>
