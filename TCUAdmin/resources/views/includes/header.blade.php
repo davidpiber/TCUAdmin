7cc0c8b13afe585ebf28dd9ddabce47a080b4e6a
David Bermudez <davidpiber@hotmail.com>
2018-02-13 20:12:51 -0600

    changes for dashbaord page

--- TCUAdmin/resources/views/includes/header.blade.php
+++ TCUAdmin/resources/views/includes/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <a class="navbar-brand" href="/principal">TCU ADMIN</a>
+          <a class="navbar-brand" href="/">TCU ADMIN</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
---
+++ TCUAdmin/resources/views/includes/info-estudiantes.blade.php
@@ -0,0 +1,18 @@
+<div class="contenedor-principal container col-md-6">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Trabajo Comunal Universitario</h3>
+    </div>
+    <div class="panel-body">
+      Texto explicando el trabajo comunal universitario.
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h3 class="panel-title">Como llenar la propuesta</h3>
+    </div>
+    <div class="panel-body">
+      Texto explicando Como llenar la propuesta, de acuerdo al cuadernillo de TCU 
+    </div>
+  </div>
+</div>
