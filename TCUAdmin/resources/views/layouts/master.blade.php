7b9d9f7b4cc29fb53a75bbc3bdd232b1cfba0cbb
David Bermudez <davidpiber@hotmail.com>
2018-02-23 21:19:36 -0600

    removiendo cdns y usando los archivos locales

--- TCUAdmin/resources/views/layouts/master.blade.php
+++ TCUAdmin/resources/views/layouts/master.blade.php
@@ -5,18 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>TCU ADMIN</title>  
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link href="{{ URL::asset('css/bootstrap.css') }}" rel="stylesheet">
         <link href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ URL::asset('css/dataTables.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class ="container-fluid">
         @include('includes.header')
             @yield('content')
         </div>
-        <script
-  src="http://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="{{ URL::asset('js/jquery.js') }}"></script>
+        <script src="{{ URL::asset('js/bootstrap.js') }}"></script>
+        <script src="{{ URL::asset('js/dataTables.js') }}"></script>
     </body>
 </html>
