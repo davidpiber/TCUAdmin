6525dcde41d2662b905557ac29942dfda7f6eb59
David Bermudez <davidpiber@hotmail.com>
2018-03-17 19:58:48 -0600

    migracion para mensajes

---
+++ TCUAdmin/database/migrations/2018_03_18_014429_crear_notas.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearNotas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('descripcion');
+            $table->string('nota');
+            $table->integer('id_proyecto_preaprobado')->unsigned();
+            $table->foreign('id_proyecto_preaprobado')->references('id')->on('proyecto_preaprobados');
+            $table->integer('id_usuario')->unsigned();
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+            $table->rememberToken();
+        });      
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario_horarios');
+    }
+}
